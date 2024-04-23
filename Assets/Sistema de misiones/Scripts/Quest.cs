2e4a66e92dce778ef51674525957dc26e9780132
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-22 22:13:33 -0300

    Creadas clases para la interfaz de misiones. Creados scriptables de Quest. Actualizada escena.

--- Assets/Sistema de misiones/Scripts/Quest.cs
+++ Assets/Sistema de misiones/Scripts/Quest.cs
@@ -9,13 +9,16 @@ namespace QuestSystem
         Principal,
         Secundaria
     }
-
+    [CreateAssetMenu(fileName = "Quest Data", menuName = "Quest System/Quest Data", order = 1)]
     public class Quest : ScriptableObject
     {
+        [SerializeField]
         string _nombre;
         public string Nombre { get => _nombre; }
+        [SerializeField]
         string _objetivo;
-        public string Objetivo { get => _objetivo;}
+        public string Objetivo { get => _objetivo; }
+        [SerializeField]
         TYPE _type;
         public TYPE Tipo { get => _type; }
     }
