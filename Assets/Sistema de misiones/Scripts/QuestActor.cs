a16cb60c8e433902f0a5571e46c8f6ef3c999044
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-22 22:51:32 -0300

    Terminado QuestActor. Creado Prefab QuestActor. Escena del Sistema de Misiones completada

--- Assets/Sistema de misiones/Scripts/QuestActor.cs
+++ Assets/Sistema de misiones/Scripts/QuestActor.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestActor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace QuestSystem {
+    public class QuestActor : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        bool callQuestManagerOnStart = false;
+        [SerializeField]
+        List<Quest> _questsToActivate = new List<Quest>();
+        [SerializeField]
+        List<Quest> _questsToCancel = new List<Quest>();
+        [SerializeField]
+        List<Quest> _questsToComplete = new List<Quest>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            if (callQuestManagerOnStart)
+            {
+                callQuestManager();
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            callQuestManager();
+        }
+
+        void callQuestManager()
+        {
+            foreach(Quest quest in _questsToActivate)
+            {
+                QuestManager.ActivarMision(quest);
+            }
+            foreach(Quest quest in _questsToCancel)
+            {
+                QuestManager.CancelarMision(quest);
+            }
+            foreach(Quest quest in _questsToComplete)
+            {
+                QuestManager.CompletarMision(quest);
+            }
+            Destroy(gameObject);
+        }
     }
 }
